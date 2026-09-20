37d8b2773d4a6295dbbdb132700ea033c281691a
Shahilsingh546 <shahilsingh456@gmail.com>
2026-09-20 17:55:04 +0530

    completed all CRUD part with type safety

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,5 @@
 import express, {NextFunction, Request, Response} from "express";
-import {any, string, z} from "zod";
+import {z} from "zod";
 import bcrypt from "bcrypt";
 import prisma from "./lib/prisma.js";
 import jwt, {JwtPayload} from "jsonwebtoken";
@@ -8,7 +8,7 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 const saltRounds = parseInt(process.env.SALT_ROUND||"10") || 10;
-const secretKey = process.env.secretKey || "mySuperSecretKey"
+const secretKey = process.env.secretKey;
 
 app.use(express.json());
 app.use(cookieParser());
@@ -16,7 +16,9 @@ app.use(cookieParser());
 const userSignUpSchema = z.object({
     name: z.string().min(3),
     username: z.string().min(5),
-    password: z.string().min(8),    
+    password: z.string().min(8),   
+    phoneNumber: z.int().optional(),
+    about: z.string().optional()
 })
 const userLogInSchema = z.object({
     username: z.string().min(5),
@@ -26,11 +28,19 @@ const userLogInSchema = z.object({
 const applicationSchema = z.object({
     companyName:z.string().min(2),
     jobPost:z.string(),
+    yoe: z.int().optional(),
+    skills: z.string().optional(),
+    packageOffer: z.string().optional(),
+    notes: z.string().optional()
 })
+
+const currentStatusSchema = z.enum(["OA", "APPLIED", "INTERVIEW", "HR", "OFFER", "REJECTED", "WITHDRAWN"])
+
 interface AuthPayload extends JwtPayload {
-    userId:string
+    userId:number
 }
-app.get("/health",userMiddleware, async(req:Request,res:Response)=>{
+
+app.get("/health", async(req:Request,res:Response)=>{
     res.json({message: `The server is up and running on port ${PORT}`});
 })
 
@@ -38,6 +48,9 @@ function userMiddleware(req:Request,res:Response,next:NextFunction){
     const token = req.cookies.token;
     if(!token){
         return res.status(401).json({msg:"User not authenticated"})
+    }
+    if(!secretKey){
+        return res.status(401).json({msg: "unauthorised request"})
     }
     try{
         const decoded = jwt.verify(token,secretKey) as AuthPayload
@@ -55,12 +68,12 @@ app.post("/signup", async(req:Request,res:Response)=>{
         if(!result.success){
             console.log(result);
             console.log("result of safeparsing->", result.success)
-            res.status(400).json({
+            return res.status(400).json({
                 msg: "Zod validation failed"
             })
         }
         else{
-            const {name, username, password, phoneNumber,about} = req.body;
+            const {name, username, password, phoneNumber,about} = result.data;
             const bodyResp = req.body;
             console.log(bodyResp);
             const hashedPassword = await bcrypt.hash(password,saltRounds);
@@ -69,20 +82,21 @@ app.post("/signup", async(req:Request,res:Response)=>{
                 name: name,
                 username : username,
                 password:hashedPassword,
-                phoneNumber: phoneNumber,
-                about:about
+                phoneNumber: phoneNumber ?? null,
+                about:about ?? null
                     }
                 })
                 console.log(resp)
             }
+            return res.json({
+                msg: "user created successfully"
+    })
         }
     catch(e){
         console.log(e);
         return res.status(500).json({msg:"something wrong"})
     }
-    res.json({
-        msg: "user created successfully"
-    })
+    
 })
 
 app.post('/login', async(req:Request,res:Response)=>{
@@ -96,16 +110,20 @@ app.post('/login', async(req:Request,res:Response)=>{
     }
     else{
         try{
-            const {username, password} = req.body;
+            const {username, password} = checkZodValidation.data;
             const resp = await prisma.user.findUnique({
                 where: {
                     username: username,
                 }
             })
+            console.log("resp-> ",resp)
             if(!resp){
                 return res.status(401).json({
                     msg: "Username not found !"
                 })
+            }
+            if(!secretKey){
+                return res.status(401).json({msg: "unauthorized request"})
             }
             else{
                 const isMatch = await bcrypt.compare(password,resp?.password || "Null")
@@ -117,7 +135,7 @@ app.post('/login', async(req:Request,res:Response)=>{
                 }
                 else{
                     console.log("before jwt sign")
-                    const token = await jwt.sign({userId:username},secretKey, {expiresIn:"7d"})
+                    const token = await jwt.sign({userId:resp.id},secretKey, {expiresIn:"7d"})
                     console.log("after jwt sign")
                     res.cookie("token", token, {
                         httpOnly:true,
@@ -127,12 +145,10 @@ app.post('/login', async(req:Request,res:Response)=>{
                     })
                     return res.status(200).json({
                         msg: "user is logged in",
-                        token: token
+
                     })
                 }
             }
-            console.log("resp for login -> ", resp)
-            
         }
         catch(e){
             console.log(e);
@@ -161,19 +177,174 @@ app.post('/application',userMiddleware, async(req:Request,res:Response)=>{
     if(!checkZodValidationOfApplication.success){
         return res.status(400).json({msg:"Invalid schema"})
     }
-    const {companyName,jobPost,yoe,skills,packageOffer,notes} = req.body;
+    const {companyName,jobPost,yoe,skills,packageOffer,notes} = checkZodValidationOfApplication.data;
     try{
-        const userId = req.userId
-        console.log("inside application route -> ",userId)
-        const user = await prisma.user.findUnique({
-            where: {
-                username:req.userId
+        const userId = req.userId;
+
+        if(!userId){
+            return res.status(401).json({msg:"unauthorized request"})
+        }
+        const respApplication = await prisma.application.create({
+            data: {
+                userId: userId,
+                companyName: companyName,
+                jobPost: jobPost,
+                yoe:yoe ?? null,
+                skills: skills ?? null,
+                packageOffer: packageOffer ?? null,
+                notes:notes ?? null
             }
+        })
+        console.log(respApplication);
+        return res.json({
+            msg:"application created successfully"
         })
 
     }
     catch{
+        res.status(500).json({msg: "something went wrong"})
+    }
+})
 
+app.get('/application', userMiddleware, async(req:Request,res:Response)=>{
+    const userId = req.userId
+    if(!userId){
+        return res.status(401).json({msg:"unauthorized user"})
+    }
+    try{
+        const resp = await prisma.application.findMany({
+            where: {
+                userId: userId
+            }
+        })
+        console.log(resp)
+        return res.json({
+            response: resp
+        })
+    }
+    catch{
+        return res.status(500).json({msg: "Something went wrong"})
+    }
+})
+
+app.get("/application/:id", userMiddleware,async(req:Request,res:Response)=>{
+    const userId = req.userId
+    if(!userId){
+        return res.status(401).json({msg:"user not authorized"})
+    }
+    const id = parseInt(req.params.id as string);
+    if(Number.isNaN(id)){
+        return res.status(400).json({msg: "please enter correct id"})
+    }
+    console.log("id -> ", id)
+    try{
+        console.log("in")
+        const application = await prisma.application.findFirst({
+        where: {
+            id: id,
+            userId: userId
+        }
+    })
+    console.log(application)
+    if(!application){
+        return res.status(404).json({msg: "application not found"})
+    }
+    return res.json({application})
+    }
+    
+    catch(e){
+        console.log(e);
+        return res.status(500).json({error:e,msg:"something went wrong"})
+    }
+})
+
+app.patch("/application/:id", userMiddleware, async(req:Request,res:Response)=>{
+    const userId = req.userId;
+    if(!userId){
+        return res.status(401).json({msg:"user not authorized"})
+    }
+    const applicationId = parseInt(req.params.id as string)
+
+    if(Number.isNaN(applicationId)){
+        return res.status(400).json({msg:"please enter a valid id"})
+    }
+    const status = currentStatusSchema.safeParse(req.body.currentStatus)
+    console.log("status schema -> ", status)
+    if(!status.success){
+        return res.status(400).json({msg:"please give correct value of currentStatus"})
+    }
+    console.log(status.data)
+    const currentStatus = status.data;
+    try{
+
+        const application = await prisma.application.findFirst({
+            where:{
+                id: applicationId,
+                userId: userId
+            }
+        })
+        if(!application){
+            return res.status(404).json({
+                msg: "application not found"
+            })
+        }
+        const applicationUpdate = await prisma.application.update({
+            where: {
+                id:application.id,
+            },
+            data: {
+                currentStatus: currentStatus
+            }
+        });
+        if(!applicationUpdate){
+            return res.status(400).json({msg: "invalid request"})
+        }
+        return res.json({msg: "successfully updated the application"})
+    }
+    catch(e){
+        console.log("error in updating data -> ", e);
+        return res.status(500).json({msg:"error in updating data"})
+    }
+})
+
+app.delete("/application/:id",userMiddleware, async(req:Request, res: Response)=>{
+    const userId = req.userId;
+    if(!userId){
+        return res.status(401).json({
+            msg: "user not authorized"
+        })
+    }
+    const applicationId = parseInt(req.params.id as string)
+
+    if(Number.isNaN(applicationId)){
+        return res.status(400).json({msg:"please enter a valid id"})
+    }
+    try{
+        console.log("userId ", userId)
+        console.log()
+        const application = await prisma.application.findFirst({
+        where: {
+            id: applicationId,
+            userId: userId
+        }
+    })
+    console.log(application)
+    if(!application){
+        return res.status(404).json({msg: "application not found"})
+    }
+    const resp = await prisma.application.delete({
+        where:{
+            id: application.id
+        }
+    })
+    if(!resp){
+        return res.status(500).json({msg: "prisma error"})
+    }
+    return res.json({msg: "successfully deleted the application"})
+    }
+    catch(e){
+        console.log(e);
+        return res.status(500).json({msg: "something went wrong"})
     }
 })
 
--- backend/src/types/express/index.d.ts
+++ backend/src/types/express/index.d.ts
@@ -3,7 +3,7 @@ import express from "express";
 declare global{
     namespace Express {
         interface Request {
-            userId?:string
+            userId?:number
         }
     }
 }
